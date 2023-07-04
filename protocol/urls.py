f5740ea13ddbb957f6b15641ede083fe7b1af5a9
user <martimanovadarina@gmail.com>
2023-07-04 20:37:18 +0200

    fixed path to main page

--- protocol/urls.py
+++ protocol/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'protocol'
+# app_name = 'protocol'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
+    # path('home/', views.home, name='home'),
     path('create_text_trans/', views.create_text_trans, name='create_text_trans'),
     path('ipfs_trans/', views.ipfs_trans, name='ipfs_trans'),
     path('about/', views.about, name='about'),
